110548e2d6c862173d3809525c47de574feeb580
Jacob Sorrell <jacoblsorrell1991@gmail.com>
2024-04-20 17:40:16 -0500

    updates to main files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+//new
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+//AuthProvider
+import { AuthContextProvider } from "./context/AuthContext";
+
+//create a client
+export const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    //provide the client to your App
+    <React.StrictMode>
+        <AuthContextProvider>
+            <QueryClientProvider client={queryClient}>
+                <App />
+                <ReactQueryDevtools />
+            </QueryClientProvider>
+        </AuthContextProvider>
+    </React.StrictMode>
+);
